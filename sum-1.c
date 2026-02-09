8d1c87e916c533424794f78170aabcfe50c11a28
Naman19266 <ns0634823@gmail.com>
2026-02-09 11:57:42 +0530

    Add files via upload

---
+++ sum-1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("I am in G2G section of B.tech-1st year/n");
+    printf("Enter the values of x and y-%d%d",x,y);
+    scanf("%d%d,&x,&y");
+    z=x+y;
+    printf("The sum of x and y is %d",z);
+    return 0;
+}
